60085a9e0513b0833bbc0975feeb8faa31b1c4ec
Sherise Leonia <sleonia@at-a1.21-school.ru>
2019-11-18 01:33:37 +0300

    define texture in define_value.h

--- includes/define_value.h
+++ includes/define_value.h
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 09:55:42 by sleonia           #+#    #+#             */
-/*   Updated: 2019/11/17 20:09:16 by sleonia          ###   ########.fr       */
+/*   Updated: 2019/11/18 01:33:21 by sleonia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,48 @@ typedef enum			e_type_boolean
 	No,
 	Yes
 }						t_type_boolean;
+
+typedef enum			e_type_texture
+{
+	T_1 = 48,
+	T_2,
+	T_3,
+	T_4,
+	T_5,
+	T_6,
+	T_7,
+	T_8,
+	T_9,
+	T_10,
+	T_11,
+	T_12,
+	T_13,
+	T_14,
+	T_15,
+	T_16,
+	T_17,
+	T_18,
+	T_19,
+	T_20,
+	T_21,
+	T_22,
+	T_23,
+	T_24,
+	T_25,
+	T_26,
+	T_27,
+	T_28,
+	T_29,
+	T_30,
+	T_31,
+	T_32,
+	T_33,
+	T_34,
+	T_35,
+	T_36,
+	T_37,
+	T_38,
+}						t_type_texture;
 
 # define WIDTH 1920
 # define HEIGHT 1080
@@ -67,12 +109,67 @@ typedef enum			e_type_boolean
 # define SCRL_BASE 		"assets/menu/XPM/scroll_base.xpm"
 # define SCRL 			"assets/menu/XPM/scroller.xpm"
 
+# define BS_TEX			"assets/textures/xpm/walls/WALL51.xpm" //DELETE
+# define WD_TEX			"assets/textures/xpm/walls/WALL96.xpm"//DELETE
+
 /*
-**	TEXTURE
+**	TEXTURE FOR BAD GUY
 */
-# define BS_TEX "assets/textures/xpm/walls/WALL96.xpm"
-# define WD_TEX	"assets/textures/xpm/object/xmastree.xpm"
-// # define BS_TEX			"assets/textures/xpm/bluestone.xpm"
-// # define WD_TEX			"assets/textures/xpm/wood.xpm"
+# define BAD_GUY		"assets/textures/xpm/bad_guy/bad_guy"
+# define BAD_GUY_2		"assets/textures/xpm/bad_guy/bad_guy_2"
+# define DEAD_GUY		"assets/textures/xpm/bad_guy/dead_guy"
+
+/*
+**	TEXTURE FOR GUNS
+*/
+# define BAZUKA			"assets/textures/xpm/gun/bazuka"
+# define PISTOLS		"assets/textures/xpm/gun/pistols"
+
+/*
+**	TEXTURE FOR GUNS ANIMATION
+*/
+# define GUN_1			"assets/textures/xpm/gun/gun_1_animation/gun_1"
+# define GUN_2			"assets/textures/xpm/gun/gun_1_animation/gun_2"
+# define GUN_3			"assets/textures/xpm/gun/gun_1_animation/gun_3"
+# define GUN_4			"assets/textures/xpm/gun/gun_1_animation/gun_4"
+# define GUN_5			"assets/textures/xpm/gun/gun_1_animation/gun_5"
+
+/*
+**	TEXTURE FOR BAD OBJECTS
+*/
+
+/*
+**	TEXTURE FOR BAD WALL
+*/
+# define WALL0			"assets/textures/xpm/walls/WALL0.xpm"
+# define WALL3			"assets/textures/xpm/walls/WALL3.xpm"
+# define WALL7			"assets/textures/xpm/walls/WALL7.xpm"
+# define WALL10			"assets/textures/xpm/walls/WALL10.xpm"
+# define WALL13			"assets/textures/xpm/walls/WALL13.xpm"
+# define WALL14			"assets/textures/xpm/walls/WALL14.xpm"
+# define WALL16			"assets/textures/xpm/walls/WALL16.xpm"
+# define WALL18			"assets/textures/xpm/walls/WALL18.xpm"
+# define WALL21			"assets/textures/xpm/walls/WALL21.xpm"
+# define WALL32			"assets/textures/xpm/walls/WALL32.xpm"
+# define WALL33			"assets/textures/xpm/walls/WALL33.xpm"
+# define WALL35			"assets/textures/xpm/walls/WALL35.xpm"
+# define WALL40			"assets/textures/xpm/walls/WALL40.xpm"
+# define WALL51			"assets/textures/xpm/walls/WALL51.xpm"
+# define WALL55			"assets/textures/xpm/walls/WALL55.xpm"
+# define WALL64			"assets/textures/xpm/walls/WALL64.xpm"
+# define WALL71			"assets/textures/xpm/walls/WALL71.xpm"
+# define WALL72			"assets/textures/xpm/walls/WALL72.xpm"
+# define WALL74			"assets/textures/xpm/walls/WALL74.xpm"
+# define WALL76			"assets/textures/xpm/walls/WALL76.xpm"
+# define WALL77			"assets/textures/xpm/walls/WALL77.xpm"
+# define WALL79			"assets/textures/xpm/walls/WALL79.xpm"
+# define WALL80			"assets/textures/xpm/walls/WALL80.xpm"
+# define WALL81			"assets/textures/xpm/walls/WALL81.xpm"
+# define WALL85			"assets/textures/xpm/walls/WALL85.xpm"
+# define WALL89			"assets/textures/xpm/walls/WALL89.xpm"
+# define WALL92			"assets/textures/xpm/walls/WALL92.xpm"
+# define WALL96			"assets/textures/xpm/walls/WALL96.xpm"
+
+//37 текстур
 
 #endif
