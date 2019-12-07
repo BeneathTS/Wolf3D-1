79731f339aaf8cd6af294afcc32f6604c99b4546
sleonia <sleonia@student.21-school.ru>
2019-12-07 20:02:12 +0300

    fix misprint on reader.c

--- sources/reader.c
+++ sources/reader.c
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 23:32:56 by sleonia           #+#    #+#             */
-/*   Updated: 2019/12/05 21:48:01 by sleonia          ###   ########.fr       */
+/*   Updated: 2019/12/07 20:00:57 by sleonia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,10 +96,10 @@ bool				read_map(const char *level_name, t_map *map)
 	fill_map(split_slash_n, split_whitespace, &map);
 	ft_destroy_string_arr(split_slash_n);
 	if ((map->width = ft_strlen(map->level[0])) < 3)
-		return (ft_free(ft_destroy_string_arr(map->level), NULL));
+		return (ft_free(map->level, NULL));
 	if (!(check_symbols_in_map(map)))
-		return (ft_free(ft_destroy_string_arr(map->level), NULL));
+		return (ft_free(map->level, NULL));
 	if (!check_borders(map))
-		return (ft_free(ft_destroy_string_arr(map->level), NULL));
+		return (ft_free(map->level, NULL));
 	return (true);
 }
