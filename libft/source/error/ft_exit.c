80439740e412e9111c58d79a5a1815e867355801
sleonia <sleonia@student.21-school.ru>
2019-12-15 10:31:01 +0300

    added gameplay images in repo

--- libft/source/error/ft_exit.c
+++ libft/source/error/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 01:57:00 by sleonia           #+#    #+#             */
-/*   Updated: 2019/12/03 12:20:14 by sleonia          ###   ########.fr       */
+/*   Updated: 2019/12/15 05:23:54 by sleonia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void		ft_exit(char *error_text)
 {
+	ft_putstr("\033[31m");
 	ft_putendl(error_text);
+	ft_putstr("\033[0m");
 	exit(1);
 }
